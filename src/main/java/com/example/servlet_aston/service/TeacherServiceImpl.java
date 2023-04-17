d2aa644185bd94c67edc1ed884358b95651626df
Ulya Ivancova <ulya-rubo@mail.ru>
2023-04-18 00:53:45 +0200

    delete

--- src/main/java/com/example/servlet_aston/service/TeacherServiceImpl.java
+++ src/main/java/com/example/servlet_aston/service/TeacherServiceImpl.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TeacherServiceImpl implements TeacherService{
+
   //  public static void main(String[] args) {
 
 //        TeacherServiceImpl teacherService = new TeacherServiceImpl();
